9943b6440550a095ff0e517e3cf10fc41ec1a109
Ankan <67393260+ankan-debug@users.noreply.github.com>
2026-09-21 00:58:37 +0530

    Format main function for better readability

--- 04-Pointers/11_array_traversal_using_pointer.c
+++ 04-Pointers/11_array_traversal_using_pointer.c
@@ -12,7 +12,18 @@
  * Run: ./program
  */
 #include <stdio.h>
-int main(void){int a[]={10,20,30,40,50};size_t n=sizeof(a)/sizeof(a[0]);int *p=a;for(size_t i=0;i<n;i++)printf("%d%s",*(p+i),i+1==n?"\n":" ");return 0;}
+
+int main(void) {
+    int a[] = {10, 20, 30, 40, 50};
+    size_t n = sizeof(a) / sizeof(a[0]);
+    int *p = a;
+
+    for (size_t i = 0; i < n; i++) {
+        printf("%d%s", *(p + i), (i + 1 == n) ? "\n" : " ");
+    }
+
+    return 0;
+}
 /*
  * Sample run:
  * 10 20 30 40 50
