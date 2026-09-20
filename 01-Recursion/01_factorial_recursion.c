a6acbd5a5813e2dc468d2579c1f8efeaf8242554
Ankan <67393260+ankan-debug@users.noreply.github.com>
2026-09-21 00:48:14 +0530

    Refactor factorial function to use int type

--- 01-Recursion/01_factorial_recursion.c
+++ 01-Recursion/01_factorial_recursion.c
@@ -11,40 +11,21 @@
  * Run: ./build/01-Recursion/01_factorial_recursion
  */
 #include <stdio.h>
-#include <limits.h>
 
-static int factorial(unsigned int n, unsigned long long *result)
-{
-    if (n == 0U || n == 1U) {
-        *result = 1ULL;
+int factorial(int n) {
+    if (n == 0 || n == 1) {
         return 1;
     }
-
-    unsigned long long previous;
-    if (!factorial(n - 1U, &previous) || previous > ULLONG_MAX / n) {
-        return 0;
-    }
-    *result = previous * n;
-    return 1;
+    else
+        return (n * factorial(n - 1));
 }
 
-int main(void)
-{
-    unsigned int number;
-    unsigned long long result;
-
+int main() {
+    int number;
     printf("Enter the number: ");
-    if (scanf("%u", &number) != 1) {
-        fprintf(stderr, "Invalid input.\n");
-        return 1;
-    }
+    scanf("%d", &number);
+    printf("Factorial of %d is %d", number, factorial(number));
 
-    if (!factorial(number, &result)) {
-        fprintf(stderr, "Factorial is too large for unsigned long long.\n");
-        return 1;
-    }
-
-    printf("Factorial of %u is %llu\n", number, result);
     return 0;
 }
 
