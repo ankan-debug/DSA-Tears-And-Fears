5f562761257ebcbdcc9d6ac5f2cc63f080c9692d
Ankan <67393260+ankan-debug@users.noreply.github.com>
2026-09-21 02:03:53 +0530

    Implement Selection Sort function
    Added a SelectionSort function and modified main to demonstrate sorting an array.

--- 07-Sorting/16_selection_sort.c
+++ 07-Sorting/16_selection_sort.c
@@ -12,8 +12,34 @@
  * Run: ./program
  */
 #include <stdio.h>
-#define MAX_SIZE 100
-int main(void){int a[MAX_SIZE];size_t n;printf("Enter number of elements: ");if(scanf("%zu",&n)!=1||n>MAX_SIZE)return 1;printf("Enter %zu elements: ",n);for(size_t i=0;i<n;i++)if(scanf("%d",&a[i])!=1)return 1;for(size_t i=0;i<n;i++){size_t m=i;for(size_t j=i+1;j<n;j++)if(a[j]<a[m])m=j;if(m!=i){int t=a[i];a[i]=a[m];a[m]=t;}}printf("Sorted Array\n");for(size_t i=0;i<n;i++)printf("%d%s",a[i],i+1==n?"\n":" ");return 0;}
+
+void SelectionSort(int array[], int size) {
+    for (int step = 0; step < size - 1; step++) {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++) {
+            if (array[i] < array[min_idx]) {
+                min_idx = i;
+            }
+        }
+        int temp = array[min_idx];
+        array[min_idx] = array[step];
+        array[step] = temp;
+    }
+}
+
+int main() {
+    int data[] = {6, 4, 7, 8, 1, 9, 5};
+    int size = sizeof(data) / sizeof(data[0]);
+    
+    SelectionSort(data, size);
+    
+    printf("Sorted Array\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    return 0;
+}
 /*
  * Sample run:
  * Enter number of elements: 7
